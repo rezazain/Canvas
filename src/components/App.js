4cdd223ca3715fc8ebbebc383b94ea49584be439
reza <fadillahreza09@gmail.com>
2022-08-22 11:37:43 +0700

    MiniApp

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../style/App.css";
+import Canvas from "./Canvas";
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas width={700} height={500} />
+    </div>
+  );
+}
+
+export default App;
